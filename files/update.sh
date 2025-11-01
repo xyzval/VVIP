1ca277a488ed69cc886587b31bab3dcc6ba593da
AUTOSC <r8149762@gmail.com>
2025-11-01 16:55:28 +0700

    Add files via upload

--- files/update.sh
+++ files/update.sh
@@ -37,8 +37,8 @@ echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
 tput cnorm
 }
 res1() {
-wget https://raw.githubusercontent.com/bowowiwendi/WendyVpn/ABSTRAK/Features/menu.zip
-wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/bowowiwendi/WendyVpn/ABSTRAK/enc/encrypt" ; chmod +x /usr/bin/enc
+wget https://raw.githubusercontent.com/xyzval/VVIP/main/Features/menu.zip
+wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/xyzval/VVIP/main/enc/encrypt" ; chmod +x /usr/bin/enc
 7z e -paskykenza123 menu.zip
 unzip menu.zip
 chmod +x menu/*
